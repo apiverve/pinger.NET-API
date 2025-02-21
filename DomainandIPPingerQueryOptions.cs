f321429dda05f9a4207e2021053b5ea6ed468074
APIVerve Team <hello@apiverve.com>
2025-02-21 07:53:53 -0600

    Added new API Endpoints

---
+++ DomainandIPPingerQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class DomainandIPPingerQueryOptions {
+public string host { get; set; }
+public int timeout { get; set; }
+}
+
+}
